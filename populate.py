037182ea4f7eddd1678f0eb0976f0d388ebc67bb
AleksandraPasternak <pasternak.a98@gmail.com>
2019-05-18 11:18:43 +0200

    Added image to recipe and bootstrap websites
    Signed-off-by: AleksandraPasternak <pasternak.a98@gmail.com>

---
+++ populate.py
@@ -0,0 +1,37 @@
+import os
+import django
+
+def populate():
+    add_tag('agrest')
+    add_tag('truskawki')
+    add_tag('maliny')
+    add_tag('porzeczki')
+    add_tag('poziomki')
+    add_tag('jablka')
+    add_tag('gruszki')
+    add_tag('jajka')
+    add_tag('rzodkiewka')
+    add_tag('ziemniaki')
+    add_tag('szpinak')
+    add_tag('cukier')
+    add_tag('kapusta')
+    add_tag('kasza')
+    add_tag('maka')
+    add_tag('pomidory')
+    add_tag('mleko')
+    add_tag('kokos')
+    add_tag('banany')
+    add_tag('liczi')
+
+
+def add_tag(name):
+    t = Tag.objects.get_or_create(name=name)
+    return t
+
+
+if __name__ == '__main__':
+    print('Populating...')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CookBook.settings')
+    django.setup()
+    from blog.models import Tag
+    populate()
